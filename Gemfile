c17bb66eec787dce1a1f0f7d72d87014848a6e1d
suzumi <ganimata39@gmail.com>
2017-09-28 22:25:55 +0900

    init commit

--- Gemfile
+++ Gemfile
@@ -52,3 +52,10 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# 認証
+gem 'devise'
+gem 'devise-i18n'
+
+# mysql
+gem 'mysql2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+
+  get 'home/authentication'
+
+  devise_for :users, module: :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root to: "home#index"
+
 end
