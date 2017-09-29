72b7d266c7c83e0b9dc126c8592a28c23ca3b0fe
suzumi <ganimata39@gmail.com>
2017-09-29 10:39:41 +0900

    fix

--- db/migrate/20170914121712_devise_create_users.rb
+++ db/migrate/20170914121712_devise_create_users.rb
@@ -1,6 +1,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
-    create_table :users do |t|
+    create_table :users, :options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4'do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
